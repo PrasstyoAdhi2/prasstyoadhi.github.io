72e7acca58136cfd3ff0cca68bbc59f7307c4114
PrasstyoAdhi2 <prasstyoadhi00@gmail.com>
2025-03-19 01:19:32 +0800

    fix

--- script.js
+++ script.js
@@ -3,4 +3,23 @@ document.addEventListener("DOMContentLoaded", function() {
     document.querySelector(".cv-container").style.animation = "fadeIn 1.5s ease-in-out";
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+    let links = document.querySelectorAll('.social-links a');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            // Get the screen width
+            const screenWidth = window.innerWidth;
+            
+            // Check if the user is on a mobile or desktop device
+            if (screenWidth <= 768) {
+                // Mobile: Change background to mobile style
+                document.body.style.backgroundImage = "url('mobileback.mp4')";
+            } else {
+                // Desktop: Change background to desktop style
+                document.body.style.backgroundImage = "url('live-background.mp4')";
+            }
+        });
+    });
+});
 
