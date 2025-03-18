6e369018d4d4a07e383a0fcbf73ae1de4bd833b4
PrasstyoAdhi2 <prasstyoadhi00@gmail.com>
2025-03-19 00:32:52 +0800

    add

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Apply the fade-in animation to the cv-container (optional, if needed)
     document.querySelector(".cv-container").style.animation = "fadeIn 1.5s ease-in-out";
 });
+
+
